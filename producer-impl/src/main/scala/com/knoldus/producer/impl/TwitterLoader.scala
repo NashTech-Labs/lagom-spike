54ebe1341a1cfae6211a515b4fb3a6faf93ea152
Tim Moore <tim.moore@lightbend.com>
2017-02-17 16:53:20 +1100

    Configure dev mode

--- producer-impl/src/main/scala/com/knoldus/producer/impl/TwitterLoader.scala
+++ producer-impl/src/main/scala/com/knoldus/producer/impl/TwitterLoader.scala
@@ -3,8 +3,9 @@ package com.knoldus.producer.impl
 import com.knoldus.producer.api.TwitterService
 import com.knoldus.producer.impl.entities.TwitterEntity
 import com.lightbend.lagom.scaladsl.api.ServiceLocator.NoServiceLocator
+import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
 import com.lightbend.lagom.scaladsl.persistence.cassandra.CassandraPersistenceComponents
-import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext, LagomApplicationLoader, LagomServer}
+import com.lightbend.lagom.scaladsl.server.{ LagomApplication, LagomApplicationContext, LagomApplicationLoader, LagomServer }
 import com.softwaremill.macwire.wire
 import play.api.libs.ws.ahc.AhcWSComponents
 
@@ -18,6 +19,8 @@ class TwitterLoader extends LagomApplicationLoader {
       override def serviceLocator = NoServiceLocator
     }
 
+  override def loadDevMode(context: LagomApplicationContext): LagomApplication =
+    new TwitterApplication(context) with LagomDevModeComponents
 }
 
 abstract class TwitterApplication(context: LagomApplicationContext) extends LagomApplication(context)
